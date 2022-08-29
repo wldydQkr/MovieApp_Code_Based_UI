ff88bdfe82393d9670396690ac98d639fde5b197
wlQkr <wlqkr1010@gmail.com>
2022-08-29 17:40:12 +0900

    Xcode Project Add

---
+++ MovieApp_CodeUI/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  MovieApp_CodeUI
+//
+//  Created by 박지용 on 2022/08/29.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MovieApp_CodeUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp_CodeUI
+//
+//  Created by 박지용 on 2022/08/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
