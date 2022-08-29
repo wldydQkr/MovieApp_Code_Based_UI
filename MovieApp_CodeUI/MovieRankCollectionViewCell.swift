b3fe7320f842436926093b61602f09127b1b04d8
wlQkr <wlqkr1010@gmail.com>
2022-08-30 01:16:06 +0900

    MovieRankCollectionViewCell 추가, SearchBar 추가

---
+++ MovieApp_CodeUI/MovieRankCollectionViewCell.swift
@@ -0,0 +1,84 @@
+//
+//  MovieRankCollectionViewCell.swift
+//  MovieApp_CodeUI
+//
+//  Created by 박지용 on 2022/08/29.
+//
+
+import UIKit
+import SnapKit
+
+final class MovieRankCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "MovieRankCollectionViewCell"
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.clipsToBounds = true
+        imageView.contentMode = .scaleToFill
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        return imageView
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .semibold)
+        
+        return label
+    }()
+    
+    private lazy var userRatingLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14.0, weight: .semibold)
+        
+        return label
+    }()
+    
+    func update() {
+        setupViews()
+        setupLayout()
+        
+        imageView.image = UIImage(systemName: "film")
+        titleLabel.text = "1. 미니언즈"
+        userRatingLabel.text = "⭐️ 10.0"
+    }
+    
+}
+
+private extension MovieRankCollectionViewCell {
+    func setupViews() {
+        layer.cornerRadius = 12.0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.1
+        layer.shadowRadius = 8.0
+        
+        backgroundColor = .systemBackground
+    }
+    
+    func setupLayout() {
+        [imageView, titleLabel, userRatingLabel]
+            .forEach { self.addSubview($0) }
+        
+        imageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16.0)
+            $0.trailing.equalToSuperview().inset(16.0)
+            $0.top.equalToSuperview().inset(16.0)
+            $0.height.equalTo(imageView.snp.width)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(8.0)
+            $0.leading.equalTo(imageView.snp.leading)
+            $0.trailing.equalTo(imageView.snp.trailing)
+        }
+        
+        userRatingLabel.snp.makeConstraints {
+            $0.leading.equalTo(imageView.snp.leading)
+            $0.trailing.equalTo(imageView.snp.trailing)
+            $0.top.equalTo(titleLabel.snp.bottom).offset(4.0)
+            $0.bottom.equalToSuperview().inset(8.0)
+        }
+    }
+}
--- MovieApp_CodeUI/MovieRankViewController.swift
+++ MovieApp_CodeUI/MovieRankViewController.swift
@@ -6,20 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 final class MovieRankViewController: UIViewController {
     
     let searchController = UISearchController()
+    
+    private lazy var collectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        
+        collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(
+            MovieRankCollectionViewCell.self,
+            forCellWithReuseIdentifier: MovieRankCollectionViewCell.identifier
+        )
+        
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigationBar()
         setupSearchBar()
+        setupViews()
     }
     
     func setupNavigationBar() {
         navigationItem.title = "박스오피스 순위"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
     }
     
@@ -27,4 +46,48 @@ final class MovieRankViewController: UIViewController {
         searchController.obscuresBackgroundDuringPresentation = false
         navigationItem.searchController = searchController
     }
+    
+    func setupViews() {
+        [collectionView]
+            .forEach { view.addSubview($0) }
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+extension MovieRankViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacing: CGFloat = 16.0
+        let width: CGFloat = (collectionView.frame.width - spacing * 3) / 2
+        return CGSize(width: width, height: 210.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        let inset: CGFloat = 16.0
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
+    
+}
+
+extension MovieRankViewController: UICollectionViewDelegate {
+    
+}
+
+extension MovieRankViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: MovieRankCollectionViewCell.identifier,
+            for: indexPath) as? MovieRankCollectionViewCell
+        
+        cell?.update()
+        
+        return cell ?? UICollectionViewCell()
+    }
+    
 }
